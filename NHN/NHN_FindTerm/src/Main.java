ac57d83fcbb0abf652c26aea6a688afcae0849ce
yearnlune <kdhpopoya@gmail.com>
2017-09-22 09:23:15 +0900

    commit

--- NHN/NHN_FindTerm/src/Main.java
+++ NHN/NHN_FindTerm/src/Main.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,7 +7,6 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		LinkedList<String> list = new LinkedList<String>();
 		int ansf = 0;
 		int anss = 0;
 		char[] vowel = {'a', 'e', 'i', 'o', 'u'};
